3c16db282f817c9482978334935be3a4abe4d672
anastasiya.krivokora <anastasiya.krivokora@quadcode.com>
2021-06-24 18:07:04 +0300

    fix bug

--- src/test/java/config/application/AppConfig.java
+++ src/test/java/config/application/AppConfig.java
@@ -3,7 +3,9 @@ package config.application;
 import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
-@Config.Sources({"classpath:config/local.properties"})
+@Config.Sources({"classpath:config/application/app_local.properties",
+        "classpath:config/app.properties"
+})
 public interface AppConfig extends Config {
     @Key("remote.web.user")
     String getRemoteWebUser();
